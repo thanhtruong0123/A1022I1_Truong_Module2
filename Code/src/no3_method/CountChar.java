194efce77b6fd0459a9b802e24bab8d2293abb44
thanhtruong0123 <thanhtruong0123@gmail.com>
2023-02-06 18:42:33 +0700

    ss03

--- Code/src/no3_method/CountChar.java
+++ Code/src/no3_method/CountChar.java
@@ -3,13 +3,13 @@ package no3_Method;
 import java.util.Scanner;
 
 public class Count_Char {
+    static  int count = 0;
     public static void main(String[] args) {
         String str = "Code Gym Da nang";
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Nhap 1 ky tu de dem");
         char countChar = sc.next().charAt(0);
-        int count = 0;
         for (int i = 0; i < str.length(); i++) {
             if (countChar == str.charAt(i)) {
                 count++;
