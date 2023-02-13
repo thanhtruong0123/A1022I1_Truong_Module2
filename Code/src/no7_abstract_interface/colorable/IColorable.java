56c9b8c7a8b91e70108ee3cdf6125caa92e05ba3
thanhtruong0123 <thanhtruong0123@gmail.com>
2023-02-13 18:34:05 +0700

    no7

---
+++ Code/src/no7_abstract_interface/colorable/IColorable.java
@@ -0,0 +1,5 @@
+package no7_abstract_interface.colorable;
+
+public interface IColorable {
+    void howToColor();
+}
---
+++ Code/src/no7_abstract_interface/colorable/Rectangle.java
@@ -0,0 +1,53 @@
+package no7_abstract_interface.colorable;
+
+public class Rectangle extends Shape implements IColorable {
+    private double length;
+    private double width;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public Rectangle(boolean filled, String color, double length, double width) {
+        super(filled, color);
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void howToColor() {
+        System.out.println("Color all four sides");
+    }
+
+    public double getArea() {
+        double area = length * width;
+        return area;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                ", area: " + getArea() +
+                ", color: " + getColor() +
+                '}';
+    }
+}
