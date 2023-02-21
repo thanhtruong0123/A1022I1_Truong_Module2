5eb48b8c002beaa3717b5f7d0b7319a42eed2461
thanhtruong0123 <thanhtruong0123@gmail.com>
2023-02-21 09:43:13 +0700

    no9

--- Code/src/no9_dsa_arraylist/array_list/MyList.java
+++ Code/src/no9_dsa_arraylist/array_list/MyList.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class MyList <E> {
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
-    private Object elements[];
+    private Object[] elements;
 
-    public MyList () {
+    public MyList() {
         elements = new Object[DEFAULT_CAPACITY];
     }
 
@@ -16,23 +16,39 @@ public class MyList <E> {
     }
 
     public void add(int index, E element) {
-        for (int i = 0; i < elements.length; i++) {
-            if (i == index) {
-                elements[i+1] = elements[i];
-                i++;
+        if (size == elements.length) {
+            ensureCapacity(size);
+        }
+
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException();
+        } else if (index == size) {
+            elements[index] = element;
+            size++;
+        } else {
+            Object[] cloneElements = elements.clone();
+
+            for (int i = index+1; i < size; i++) {
+                elements[i] = cloneElements[i-1];
             }
-            elements[i] = element;
+            elements[index] = element;
             size++;
         }
     }
 
     public E remove(int index) {
-        for (int i = 0; i < elements.length; i++) {
-            if (i == index) {
-                elements[i-1] = elements[i];
-                i++;
-            }
+        if (index <= 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        } else if (index == size) {
+            elements[index] = null;
             size--;
+        } else {
+            Object[] cloneElements = elements.clone();
+
+            for (int i = index; i < size; i++) {
+                elements[i] = cloneElements[i+1];
+                size--;
+            }
         }
         return null;
     }
@@ -41,62 +57,45 @@ public class MyList <E> {
         return size;
     }
 
-    public E clone() {
-        return null;
-    }
+//    public E clone() {}
 
-    public boolean contains(E o) {
-        boolean isTrue = false;
-        for (Object element : elements) {
-            if (element == o) {
-                isTrue = true;
+    public boolean contains (E o) {
+        boolean result = false;
+
+        for (int i = 0; i < size; i++) {
+            if (o == elements[i]) {
+                result = true;
                 break;
             }
         }
-        return isTrue;
+
+        return result;
     }
 
     public int indexOf(E o) {
-        int index = 0;
-        for (int i = 0; i < elements.length; i++) {
-            if (elements[i] == o) {
-                System.out.println(i);
-                index = i;
+        int result = -1;
+
+        for (int i = 0; i < size; i++) {
+            if (o == elements[i]) {
+                result = i;
                 break;
             }
         }
-        return index;
+
+        return result;
     }
 
-    public boolean add(E e) {
-        if (size == elements.length) {
-            ensureCapacity();
+//    public boolean add(E element) {
+//    }
+
+    public void ensureCapacity(int minCapacity) {
+        if (minCapacity == elements.length) {
+            Object[] newElements = Arrays.copyOf(elements,elements.length* 2);
+            elements = newElements;
         }
-        elements[size++] = e;
-        return true;
-    }
-
-    public void ensureCapacity() {
-        int newSize = elements.length * 2;
-        elements = Arrays.copyOf(elements, newSize);
     }
 
     public E get(int i) {
-        return  (E) elements[i];
-    }
-
-    public void clear() {
-        for (int i = 0; i < elements.length; i++) {
-            elements[i] = 0;
-        }
-        size = 0;
-    }
-
-    @Override
-    public String toString() {
-        return "MyList{" +
-                "size=" + size +
-                ", elements=" + Arrays.toString(elements) +
-                '}';
+        return (E) elements[i];
     }
 }
--- Code/src/no9_dsa_arraylist/array_list/MyListTest.java
+++ Code/src/no9_dsa_arraylist/array_list/MyListTest.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 
 public class MyListTest {
     public static void main(String[] args) {
-        MyList myList = new MyList();
-        myList.add(8);
-        myList.add(9);
+        MyList<Integer> myList = new MyList<Integer>();
+
+        myList.add(0,1);
+        myList.add(1,2);
+        myList.add(2,3);
+
+        myList.remove(1);
+
+        
 
         for (int i = 0; i < myList.size(); i++) {
             System.out.println(myList.get(i));
