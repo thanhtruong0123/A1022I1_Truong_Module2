4822149c35d130af5fa7be8385fa908fb0b17e7f
thanhtruong0123 <thanhtruong0123@gmail.com>
2023-02-23 22:31:45 +0700

    no11

--- Code/src/no10_dsa_stack_queue/count_str/StringCounting.java
+++ Code/src/no10_dsa_stack_queue/count_str/StringCounting.java
@@ -1,5 +1,34 @@
 package no10_dsa_stack_queue.count_str;
 
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class StringCounting {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a document to count word");
+        String document = sc.nextLine();
+
+        String[] arr = document.split(" ");
+
+        Map<String, Integer> myMap = new TreeMap<>();
+
+
+        for (int i = 0; i < arr.length; i++) {
+            String word = arr[i];
+            int count = 0;
+
+            for (String element : arr) {
+                if (element.equals(word)) {
+                    count++;
+                }
+                myMap.put(word, count);
+            }
+        }
+
+        for (String key: myMap.keySet()) {
+            System.out.println(key + " : " + myMap.get(key) + " time(s)");
+        }
+    }
 }
